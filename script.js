4f7999bf37f2614301a229f4310e9826195a64bc
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-04-22 12:52:05 -0500

    finsihed challenge

---
+++ script.js
@@ -0,0 +1,27 @@
+let arrayS = ["Hello", "Hi", "Hey", "Salutations", "Oloha"];
+let arrayN = [87, 34, 67, 87, 90];
+let arrayB = [true, false, true, false, true];
+
+function arrayAdder(p1)
+{
+    if (typeof(p1[0]) == "number"){
+
+        p1.push(Number(88))
+        return console.log(p1)
+
+    }
+    else if (typeof(p1[0]) == "string"){
+        p1.push("Barack")
+        return console.log(p1)
+    }
+    else if (typeof(p1[0]) == "boolean"){
+        p1.push(false)
+        return console.log(p1)
+        
+    }
+    else {
+        console.log("RETURN A VALID DATA TYPE FOR YOUR '0' INDEX")
+    }
+}
+
+arrayAdder(arrayB)
